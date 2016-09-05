382b743c2e74abfb627ec4b67a4889fb4a4508c0
Nicola Verbeeck <nicola@chimerapps.com>
2016-09-05 09:03:08 +0200

    Set drag listener on item

--- library/src/main/java/com/woxthebox/draglistview/DragItemAdapter.java
+++ library/src/main/java/com/woxthebox/draglistview/DragItemAdapter.java
@@ -25,167 +25,165 @@ import java.util.List;
 
 public abstract class DragItemAdapter<T, VH extends DragItemAdapter.ViewHolder> extends RecyclerView.Adapter<VH> {
 
-    interface DragStartCallback {
-        boolean startDrag(View itemView, long itemId);
-        boolean isDragging();
-    }
+	interface DragStartCallback {
+		boolean startDrag(View itemView, long itemId);
 
-    private DragStartCallback mDragStartCallback;
-    private long mDragItemId = RecyclerView.NO_ID;
-    private long mDropTargetId = RecyclerView.NO_ID;
-    private boolean mDragOnLongPress;
-    protected List<T> mItemList;
+		boolean isDragging();
+	}
 
-    public DragItemAdapter(boolean dragOnLongPress) {
-        mDragOnLongPress = dragOnLongPress;
-    }
+	private DragStartCallback mDragStartCallback;
+	private long mDragItemId = RecyclerView.NO_ID;
+	private long mDropTargetId = RecyclerView.NO_ID;
+	private boolean mDragOnLongPress;
+	protected List<T> mItemList;
 
-    public void setItemList(List<T> itemList) {
-        mItemList = itemList;
-        notifyDataSetChanged();
-    }
+	public DragItemAdapter(boolean dragOnLongPress) {
+		mDragOnLongPress = dragOnLongPress;
+	}
 
-    public List<T> getItemList() {
-        return mItemList;
-    }
+	public void setItemList(List<T> itemList) {
+		mItemList = itemList;
+		notifyDataSetChanged();
+	}
 
-    public Object removeItem(int pos) {
-        if (mItemList != null && mItemList.size() > pos) {
-            Object item = mItemList.remove(pos);
-            notifyItemRemoved(pos);
-            return item;
-        }
-        return null;
-    }
+	public List<T> getItemList() {
+		return mItemList;
+	}
 
-    public void addItem(int pos, T item) {
-        if (mItemList != null && mItemList.size() >= pos) {
-            mItemList.add(pos, item);
-            notifyItemInserted(pos);
-        }
-    }
+	public Object removeItem(int pos) {
+		if (mItemList != null && mItemList.size() > pos) {
+			Object item = mItemList.remove(pos);
+			notifyItemRemoved(pos);
+			return item;
+		}
+		return null;
+	}
 
-    public void changeItemPosition(int fromPos, int toPos) {
-        if (mItemList != null && mItemList.size() > fromPos && mItemList.size() > toPos) {
-            T item = mItemList.remove(fromPos);
-            mItemList.add(toPos, item);
-            notifyItemMoved(fromPos, toPos);
-        }
-    }
+	public void addItem(int pos, T item) {
+		if (mItemList != null && mItemList.size() >= pos) {
+			mItemList.add(pos, item);
+			notifyItemInserted(pos);
+		}
+	}
 
-    public void swapItems(int pos1, int pos2) {
-        if (mItemList != null && mItemList.size() > pos1 && mItemList.size() > pos2) {
-            Collections.swap(mItemList, pos1, pos2);
-            notifyDataSetChanged();
-        }
-    }
+	public void changeItemPosition(int fromPos, int toPos) {
+		if (mItemList != null && mItemList.size() > fromPos && mItemList.size() > toPos) {
+			T item = mItemList.remove(fromPos);
+			mItemList.add(toPos, item);
+			notifyItemMoved(fromPos, toPos);
+		}
+	}
 
-    public int getPositionForItemId(long id) {
-        int count = getItemCount();
-        for (int i = 0; i < count; i++) {
-            if (id == getItemId(i)) {
-                return i;
-            }
-        }
-        return RecyclerView.NO_POSITION;
-    }
+	public void swapItems(int pos1, int pos2) {
+		if (mItemList != null && mItemList.size() > pos1 && mItemList.size() > pos2) {
+			Collections.swap(mItemList, pos1, pos2);
+			notifyDataSetChanged();
+		}
+	}
 
-    @Override
-    public int getItemCount() {
-        return mItemList == null ? 0 : mItemList.size();
-    }
+	public int getPositionForItemId(long id) {
+		int count = getItemCount();
+		for (int i = 0; i < count; i++) {
+			if (id == getItemId(i)) {
+				return i;
+			}
+		}
+		return RecyclerView.NO_POSITION;
+	}
 
-    @Override
-    public void onBindViewHolder(VH holder, int position) {
-        long itemId = getItemId(position);
-        holder.mItemId = itemId;
-        holder.itemView.setVisibility(mDragItemId == itemId ? View.INVISIBLE : View.VISIBLE);
-    }
+	@Override
+	public int getItemCount() {
+		return mItemList == null ? 0 : mItemList.size();
+	}
 
-    void setDragStartedListener(DragStartCallback dragStartedListener) {
-        mDragStartCallback = dragStartedListener;
-    }
+	@Override
+	public void onBindViewHolder(VH holder, int position) {
+		long itemId = getItemId(position);
+		holder.mItemId = itemId;
+		holder.itemView.setVisibility(mDragItemId == itemId ? View.INVISIBLE : View.VISIBLE);
+	}
 
-    void setDragItemId(long dragItemId) {
-        mDragItemId = dragItemId;
-    }
+	void setDragStartedListener(DragStartCallback dragStartedListener) {
+		mDragStartCallback = dragStartedListener;
+	}
 
-    void setDropTargetId(long dropTargetId) {
-        mDropTargetId = dropTargetId;
-    }
+	void setDragItemId(long dragItemId) {
+		mDragItemId = dragItemId;
+	}
 
-    public long getDropTargetId() {
-        return mDropTargetId;
-    }
+	void setDropTargetId(long dropTargetId) {
+		mDropTargetId = dropTargetId;
+	}
 
-    public abstract class ViewHolder extends RecyclerView.ViewHolder {
-        public View mGrabView;
-        public long mItemId;
+	public long getDropTargetId() {
+		return mDropTargetId;
+	}
 
-        public ViewHolder(final View itemView, int handleResId) {
-            super(itemView);
-            mGrabView = itemView.findViewById(handleResId);
+	public abstract class ViewHolder extends RecyclerView.ViewHolder {
+		public View mGrabView;
+		public long mItemId;
 
-            if (mDragOnLongPress) {
-                mGrabView.setOnLongClickListener(new View.OnLongClickListener() {
-                    @Override
-                    public boolean onLongClick(View view) {
-                        if (mDragStartCallback.startDrag(itemView, mItemId)) {
-                            return true;
-                        }
-                        if (itemView == mGrabView) {
-                            return onItemLongClicked(view);
-                        }
-                        return false;
-                    }
-                });
-            } else {
-                mGrabView.setOnTouchListener(new View.OnTouchListener() {
-                    @Override
-                    public boolean onTouch(View view, MotionEvent event) {
-                        if (event.getAction() == MotionEvent.ACTION_DOWN && mDragStartCallback.startDrag(itemView, mItemId)) {
-                            return true;
-                        }
-                        if (!mDragStartCallback.isDragging() && itemView == mGrabView) {
-                            return onItemTouch(view, event);
-                        }
-                        return false;
-                    }
-                });
-            }
+		public ViewHolder(final View itemView, int handleResId) {
+			super(itemView);
+			mGrabView = itemView.findViewById(handleResId);
 
-            itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    onItemClicked(view);
-                }
-            });
+			if (mDragOnLongPress) {
+				itemView.setOnLongClickListener(new View.OnLongClickListener() {
+					@Override
+					public boolean onLongClick(View view) {
+						if (mDragStartCallback.startDrag(itemView, mItemId)) {
+							return true;
+						}
+						return onItemLongClicked(view);
+					}
+				});
+			}
+			mGrabView.setOnTouchListener(new View.OnTouchListener() {
+				@Override
+				public boolean onTouch(View view, MotionEvent event) {
+					if (event.getAction() == MotionEvent.ACTION_DOWN && mDragStartCallback.startDrag(itemView, mItemId)) {
+						return true;
+					}
+					if (!mDragStartCallback.isDragging() && itemView == mGrabView) {
+						return onItemTouch(view, event);
+					}
+					return false;
+				}
+			});
 
-            if (itemView != mGrabView) {
-                itemView.setOnLongClickListener(new View.OnLongClickListener() {
-                    @Override
-                    public boolean onLongClick(View view) {
-                        return onItemLongClicked(view);
-                    }
-                });
-                itemView.setOnTouchListener(new View.OnTouchListener() {
-                    @Override
-                    public boolean onTouch(View view, MotionEvent event) {
-                        return onItemTouch(view, event);
-                    }
-                });
-            }
-        }
 
-        public void onItemClicked(View view) {
-        }
+			itemView.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					onItemClicked(view);
+				}
+			});
 
-        public boolean onItemLongClicked(View view) {
-            return false;
-        }
+			if (itemView != mGrabView) {
+				itemView.setOnLongClickListener(new View.OnLongClickListener() {
+					@Override
+					public boolean onLongClick(View view) {
+						return onItemLongClicked(view);
+					}
+				});
+				itemView.setOnTouchListener(new View.OnTouchListener() {
+					@Override
+					public boolean onTouch(View view, MotionEvent event) {
+						return onItemTouch(view, event);
+					}
+				});
+			}
+		}
 
-        public boolean onItemTouch(View view, MotionEvent event) {
-            return false;
-        }
-    }
+		public void onItemClicked(View view) {
+		}
+
+		public boolean onItemLongClicked(View view) {
+			return false;
+		}
+
+		public boolean onItemTouch(View view, MotionEvent event) {
+			return false;
+		}
+	}
 }
